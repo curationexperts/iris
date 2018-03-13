f7cb1e5c637d0226f757c9180bf87dd5e019b018
Jennifer Lindner <jenlindner@gmail.com>
2018-03-13 11:15:18 -0400

    correcting actor class names in specs and correcting names of controller spec files

--- spec/actors/raster_work_actor_spec.rb
+++ spec/actors/raster_work_actor_spec.rb
@@ -2,7 +2,7 @@
 #  `rails generate geo_works:install`
 require 'rails_helper'
 
-describe Hyrax::RasterWorkActor do
+describe Hyrax::Actors::RasterWorkActor do
   it "has tests" do
     skip "Add your tests here"
   end
