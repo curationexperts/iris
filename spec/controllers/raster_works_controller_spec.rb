a588bb047778bb6efcb09b6817ca16ea9b03e5af
val99erie <val99erie@gmail.com>
2018-03-15 15:51:05 -0500

    Removed generated pending specs

--- spec/controllers/raster_works_controller_spec.rb
+++ spec/controllers/raster_works_controller_spec.rb
@@ -1,9 +1,9 @@
 # Generated via
 #  `rails generate geo_works:install`
-require 'rails_helper'
+# require 'rails_helper'
 
-describe Hyrax::RasterWorksController do
-  it "has tests" do
-    skip "Add your tests here"
-  end
-end
+# describe Hyrax::RasterWorksController do
+#   it "has tests" do
+#     skip "Add your tests here"
+#   end
+# end
--- spec/models/image_work_spec.rb
+++ spec/models/image_work_spec.rb
@@ -1,9 +1,9 @@
 # Generated via
 #  `rails generate geo_works:install`
-require 'rails_helper'
+# require 'rails_helper'
 
-describe ImageWork do
-  it "has tests" do
-    skip "Add your tests here"
-  end
-end
+# describe ImageWork do
+#   it "has tests" do
+#     skip "Add your tests here"
+#   end
+# end
