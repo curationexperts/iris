98e106f383bfdf3c1d6605ed57a15027309c0bf1
Jennifer Lindner <jenlindner@gmail.com>
2018-03-16 10:12:28 -0400

    renaming app settings to iris

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module UcsbGeoWorks
+module Iris
   class Application < Rails::Application
     config.active_job.queue_adapter     = :sidekiq
 
