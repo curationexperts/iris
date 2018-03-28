a84b248fcaedc5698e039029141fd86959847dbc
Jennifer Lindner <jenlindner@gmail.com>
2018-03-28 14:45:49 -0400

    adding some mappings for blacklight fields

--- app/importers/iris_mapper.rb
+++ app/importers/iris_mapper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class IrisMapper < Darlingtonia::HashMapper
   def fields
-    [:creator, :title, :keyword, :rights, :resource_type, :visibility, :representative_file, :shapefiles, :geo_mime_type]
+    [:creator, :title, :keyword, :rights, :resource_type, :visibility, :representative_file, :shapefiles, :geo_mime_type, :spatial, :temporal, :provenance]
   end
 
   def visibility
@@ -16,8 +16,20 @@ class IrisMapper < Darlingtonia::HashMapper
     metadata['shape_file']
   end
 
+  def provenance
+    metadata['provenance']
+  end
+
+  def spatial
+    [metadata['spatial']]
+  end
+
+  def temporal
+    [metadata['temporal']]
+  end
   # we need to send the geo-derivatives class double-quoted strings, but csv input works out of the box with single quotes. this method replaces singles in the csv with doubles.
   # TODO: we might want to use a validator to enforce single quotes in the csvs instead.
+
   def geo_mime_type
     return if metadata['geo_mime_type'].nil?
     metadata['geo_mime_type'].tr("'", "\"")
