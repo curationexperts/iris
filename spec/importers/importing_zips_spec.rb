6df5e40c8d707271c392cb4e412b38e26a185d8f
val99erie <val99erie@gmail.com>
2018-04-26 09:58:54 -0500

    Moved some end-to-end specs to the zip importer from the CSV importer, in preparation for removing the CSV importer

--- spec/importers/importing_zips_spec.rb
+++ spec/importers/importing_zips_spec.rb
@@ -8,16 +8,27 @@ RSpec.describe 'Importing from a directory of zip files:' do
   let(:zip_path) { File.join(fixture_path, 'import_zips_in_dir') }
   let(:rec_importer) { ZipRecordImporter.new }
 
-  context 'a raster and a vector work' do
+  # We use 'perform_enqueued' because GeoserverDeliveryJob
+  # only gets queued during background jobs, not during import.
+  context 'a raster and a vector work', :perform_enqueued do
     let(:raster) { RasterWork.first }
     let(:vector) { VectorWork.first }
 
     before do
+      ActiveJob::Base.queue_adapter.filter = [IngestFileJob, CharacterizeJob, CreateDerivativesJob]
+      # Don't run fits during the specs:
+      allow(Hydra::Works::CharacterizationService).to receive(:run)
+
       expect(RasterWork.count).to eq 0
       expect(VectorWork.count).to eq 0
     end
 
     it 'creates the works with the files attached' do
+      # It should queue jobs to update the
+      # GeoBlacklight app & GeoServer
+      expect(GeoblacklightJob).to receive(:perform_later).twice
+      expect(GeoserverDeliveryJob).to receive(:perform_later).twice
+
       expect { importer.import }
         .to change { RasterWork.count }.by(1)
         .and change { VectorWork.count }.by(1)
@@ -32,6 +43,7 @@ RSpec.describe 'Importing from a directory of zip files:' do
       # TIFF file should be representative file
       expect(tif_file.geo_mime_type).to eq 'image/tiff; gdal-format=GTiff'
       expect(tif_file.label).to eq 'gford-20140000-010045_rbmgrd-t.tif'
+      expect(tif_file.thumbnail_id).not_to be_empty
       expect(raster.representative_id).to eq tif_file.id
       expect(tif_file.visibility).to eq 'open'
 
@@ -44,8 +56,44 @@ RSpec.describe 'Importing from a directory of zip files:' do
       expect(vector.ordered_members.to_a.size).to eq 1
       shapefile = vector.ordered_members.to_a.first
       expect(shapefile.geo_mime_type).to eq 'application/zip; ogr-format="ESRI Shapefile"'
+      expect(shapefile.thumbnail_id).not_to be_empty
       expect(vector.representative_id).to eq shapefile.id
       expect(shapefile.visibility).to eq 'open'
+    end
+  end
+
+  describe 'Generated URLs' do
+    let(:zip_path) { File.join(fixture_path, 'single_zip') }
+    let(:expected_reference) { /"#{expected_protocol}#{ENV['RAILS_HOST']}\/concern\/raster_works\/#{raster.id}"/ }
+    let(:raster) { RasterWork.first } # the newly-created record
+
+    before do
+      ActiveJob::Base.queue_adapter.enqueued_jobs.clear
+      allow(Rails.application.config).to receive(:force_ssl).and_return(ssl_config)
+      importer.import
+      expect(RasterWork.count).to eq 1
+    end
+
+    context 'When SSL is configured' do
+      let(:ssl_config) { true }
+      let(:expected_protocol) { 'https://' }
+
+      it 'configures URL with https' do
+        expect(GeoblacklightJob).to have_been_enqueued.with(
+          hash_including('doc' => hash_including(dct_references_s: expected_reference))
+        )
+      end
+    end
+
+    context 'When SSL is not configured' do
+      let(:ssl_config) { false }
+      let(:expected_protocol) { 'http://' }
+
+      it 'configures URL with http' do
+        expect(GeoblacklightJob).to have_been_enqueued.with(
+          hash_including('doc' => hash_including(dct_references_s: expected_reference))
+        )
+      end
     end
   end
 end
